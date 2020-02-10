d1db93eb0511d1b3a98b20afd071f11efbfb5f29
Jacob <tkwon2@sva.edu>
2020-02-10 15:11:26 -0500

    add npm

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 var button = document.querySelector('.button')
 var inputValue = document.querySelector('.inputValue')
 var name = document.querySelector('.name')
+var temp = document.querySelector('.temp')
 var desc = document.querySelector('.desc')
 
 
-button.addEventListener('click',function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=1bc4eb7927d34346d10dd2bfacb702f0')
-    .then(response => response.json())
-    .then(data => {
-        var nameValue = data['name'];
-        var descValue = data['weather'][0]['description']
 
-        name.innerHTML = nameValue;
-        desc.innerHTML = descValue;
+button.addEventListener('click', function () {
 
-    })
-    
-    
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&appid=1bc4eb7927d34346d10dd2bfacb702f0')
+        .then(response => response.json())
+        .then(data => {
+            var nameValue = data['name'];
+            var tempValue = data['main']['temp'];
+            var descValue = data['weather'][0]['description']
 
-    .catch(err => alert("Wrong Name!"))
-    
-})
+            let realTemp = ((tempValue - 273.15) * 9/5) + 32
+            var farenheit = realTemp.toFixed() +' F';
+
+            name.innerHTML = nameValue;
+            temp.innerHTML = farenheit;
+            desc.innerHTML = descValue;
+
+        })
 
 
+
+        .catch(err => alert("Wrong Name!"))
+
+}, function animateCSS(){
+    const searchBar = document.querySelector('.bar')
+    searchBar.classList.add('animated','bounce')
+});

a6ca28c4a6d6a0e239ac2fc745aff01f628a374c
Jacob <tkwon2@sva.edu>
2020-02-17 13:23:00 -0500

    add sass

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var inputValue = document.querySelector('.inputValue')
 var name = document.querySelector('.name')
 var temp = document.querySelector('.temp')
 var desc = document.querySelector('.desc')
+var icon = document.querySelector('.icon')
+
 
 
 
@@ -12,23 +14,32 @@ button.addEventListener('click', function () {
         .then(response => response.json())
         .then(data => {
             var nameValue = data['name'];
-            var tempValue = data['main']['temp'];
-            var descValue = data['weather'][0]['description']
 
+            var tempValue = data['main']['temp'];
             let realTemp = ((tempValue - 273.15) * 9/5) + 32
-            var farenheit = realTemp.toFixed() +' F';
+
+            var descValue = data['weather'][0]['description'];
+
+            var iconValue = data['weather'][0]['icon'];
+            var iconUrl = "http://openweathermap.org/img/w/" + iconValue + ".png";
+
+            console.log(iconUrl)
 
             name.innerHTML = nameValue;
-            temp.innerHTML = farenheit;
+            temp.innerHTML = realTemp.toFixed() +' F';
             desc.innerHTML = descValue;
+            icon.innerHTM = iconUrl;
 
         })
-
-
-
         .catch(err => alert("Wrong Name!"))
 
-}, function animateCSS(){
-    const searchBar = document.querySelector('.bar')
+        animateCSS()
+        
+}); 
+
+
+
+function animateCSS(){
+    const searchBar = document.querySelector('#search')
     searchBar.classList.add('animated','bounce')
-});
+}
